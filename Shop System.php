430b76533087e604b5eb606f8ff4f3a0b451c779
MauricioDoria <mur1kin44@gmail.com>
2025-10-25 15:06:31 -0300

    added payment system

--- Shop System.php
+++ Shop System.php
@@ -19,5 +19,33 @@ $product = (float)$product ;
 echo 'Fim de compra' . PHP_EOL;
 echo "Voce comprou $itens itens e ao todo sua compra deu $product reais";
 
-$prodplus = 's';
+#Payment System | Sistema de Pagamento
+
+echo "Qual a forma de pagamento :\n[0] Credit Card / Debit Card\n[1] Cash Payment\n[2] Store Card : "; $paymentOp = (int)trim(fgets(STDIN));
+
+while ($paymentOp != 0 && $paymentOp != 1 && $paymentOp != 2){
+    echo 'Opção INvalida';
+    echo "Qual a forma de pagamento :\n[0] Credit Card / Debit Card\n[1] Cash Payment\n[2] Store Card : "; $paymentOp = (int)trim(fgets(STDIN));
+
+}
+if ($paymentOp == 0){
+    echo "[0]Credit Card (6x)\n[1]Debit Card : " ; $cardOp = (int)trim(fgets(STDIN));
+    if ($cardOp == 0 ){
+        echo 'Opção Indisponivel';
+    } elseif ($cardOp == 1) {
+        echo "Opção não disponivel";
+    }
+}
+elseif ($paymentOp == 2){
+    echo "Using the store card you get 15% off on purchases over $50 and 10% off on purchases below that." . PHP_EOL;
+    if ($product > 50){
+        $product= $product-($product * 0.15);
+    } else{ 
+        $product = $product-($product * 0.10);
+    }
+    echo "The final value of your purchase was $product ";
+}
+
+echo 'Purchase completed...' . PHP_EOL . 'Thank the customer "Thank you for your preference"' ;
+
 ?>
