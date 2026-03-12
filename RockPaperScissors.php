4b145e3f952ec46a1cff0da5244587458bb219cc
MauricioDoria <mur1kin44@gmail.com>
2026-03-12 18:10:58 -0300

    Create RockPaperScissors.php

---
+++ RockPaperScissors.php
@@ -0,0 +1,23 @@
+<?php 
+$opção  = ["PEDRA", "PAPEL", "TESOURA"];#Config IA
+$ia = random_int(0,2);
+if ($ia == 0){$ia='PEDRA';}elseif($ia == 1){$ia = 'PAPEL' ;}else{$ia = 'TESOURA';}
+echo $ia;
+
+echo 'O que irar jogar' . PHP_EOL;($player=(string)trim(fgets(STDIN)));
+$player = strtoupper($player);
+
+while (in_array("$player", $opção) == false){
+    echo 'O que irar jogar' . PHP_EOL;$player=(string)trim(fgets(STDIN));
+    $player = strtoupper($player);
+}
+echo 'tudo certo' . PHP_EOL; sleep(2);
+
+if($ia == $player){echo "Ambos jogaram $ia o jogo deu Empate";}
+else{
+    if ($ia == 'PEDRA' and $player == 'TESOURA' or $ia == 'TESOURA' and $player == 'PAPEL' or $ia == 'PAPEL' and $player == 'PEDRA'){
+        echo "Voce PERDEU\n"; 
+    } else{echo "Voce GANHOU";}
+}
+echo "A IA JOGOU $ia e voce jogou $player";
+?>
