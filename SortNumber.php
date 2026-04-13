6d69c5c5cfcc5faf0c7ab40af968c9c51b98ac38
MauricioDoria <mur1kin44@gmail.com>
2026-04-13 18:31:47 -0300

    Update SortNumber.php

--- SortNumber.php
+++ SortNumber.php
@@ -1,13 +1,13 @@
 <?php 
 
 $NumberSort = random_int(0,10);
-
+echo 'TENTE ADVINHAR UM NUMERO DE 0 A 10' . PHP_EOL;
 echo 'Tente advinhar o numero com 5 Tentativas' . PHP_EOL;
 $cont = 1 ; 
 while (true){
     
     echo "$cont ª TENTATIVA : " ; $tentativa = trim(fgets(STDIN));
-    if(is_numeric($tentativa)){
+    if(is_numeric($tentativa) and $tentativa >= 0 and $tentativa <= 10){
         if ($tentativa < 10){
             if ($tentativa == $NumberSort){
                 echo 'Você acertou o numero' . PHP_EOL ;
@@ -27,6 +27,7 @@ while (true){
     else{
         echo 'Numero invalido... tente novamente: ' . PHP_EOL;
         echo "$cont ª TENTATIVA : " ; $tentativa = (int)trim(fgets(STDIN));
+        $cont++;
     }
     if ($cont > 5){
         echo 'Voce usou todas as suas chances' . PHP_EOL;
