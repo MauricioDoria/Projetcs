7c37ab287b58fb7b6b30d6365993da0d39baee41
MauricioDoria <mur1kin44@gmail.com>
2026-04-15 19:44:47 -0300

    Update Var

--- School_Registration.php
+++ School_Registration.php
@@ -15,31 +15,31 @@ while (true){
         "Media" => 0
     ];
 
-    echo "Deseja cadastrar um novo aluno: [S] | [N] " ; $op = trim(fgets(STDIN));
-    if (is_string($op)){
-        if (strtoupper($op) == 'N'){
+    echo "Deseja cadastrar um novo aluno: [S] | [N] " ; $Option = trim(fgets(STDIN));
+    if (is_string($Option)){
+        if (strtoupper($Option) == 'N'){
             break;
         }
-        elseif(strtoupper($op) == 'S'){
+        elseif(strtoupper($Option) == 'S'){
             continue;
         }
         else{
             echo 'Texto Invalido: ';
-            echo "Deseja cadastrar um novo aluno: [S] | [N] " ; $op = trim(fgets(STDIN));
+            echo "Deseja cadastrar um novo aluno: [S] | [N] " ; $Option = trim(fgets(STDIN));
         }
     } else {
         echo 'Texto Invalido: ';
-        echo "Deseja cadastrar um novo aluno: [S] | [N] " ; $op = trim(fgets(STDIN));
-        if (strtoupper($op[0]) == 'N' || strtoupper($op[0]) != 'S'){
+        echo "Deseja cadastrar um novo aluno: [S] | [N] " ; $Option = trim(fgets(STDIN));
+        if (strtoupper($Option[0]) == 'N' || strtoupper($Option[0]) != 'S'){
             break;
         }
     }
 }
 
-echo "Adicionar MEDIA a aluno: [S] | [N] " ; $op = trim(fgets(STDIN));
+echo "Adicionar MEDIA a aluno: [S] | [N] " ; $Option = trim(fgets(STDIN));
 
-if (is_string($op)){
-    if (strtoupper($op) == 'S'){
+if (is_string($Option)){
+    if (strtoupper($Option) == 'S'){
         echo 'Qual Aluno deseja fazer esta alteração: '; $alunomedia=trim(fgets(STDIN));
         if (!isset($cadastro[$alunomedia])){
             echo "Erro. FECHANDO PROGRAMA";
@@ -47,31 +47,31 @@ if (is_string($op)){
                 $nota = 0;
                 echo 'Digite todas as notas do aluno: ' . PHP_EOL; $media=1;
                 echo "Nota $media: " ;$n=(float)trim(fgets(STDIN));
-                echo 'MAIS UMA NOTA: [S] | [N] ' ; $op = trim(fgets(STDIN));
-                if(is_string($op)){
-                    if (strtoupper($op[0]) != 'S' && strtoupper($op[0]) != 'N'){
-                        echo 'MAIS UMA NOTA: [S] | [N] ' ; $op = trim(fgets(STDIN));
+                echo 'MAIS UMA NOTA: [S] | [N] ' ; $Option = trim(fgets(STDIN));
+                if(is_string($Option)){
+                    if (strtoupper($Option[0]) != 'S' && strtoupper($Option[0]) != 'N'){
+                        echo 'MAIS UMA NOTA: [S] | [N] ' ; $Option = trim(fgets(STDIN));
                     }
-                    elseif (strtoupper($op[0]) == 'S') {
-                        while (strtoupper($op) != 'N'){
+                    elseif (strtoupper($Option[0]) == 'S') {
+                        while (strtoupper($Option) != 'N'){
                             $media++;
-                            echo "Nota $media: " ;$n=(float)trim(fgets(STDIN));
+                            echo "Nota $media: " ;$nz=(float)trim(fgets(STDIN));
                             $nota+=$n;
                             
-                            echo 'MAIS UMA NOTA: [S] | [N] ' ; $op = trim(fgets(STDIN));
-                            if(is_string($op)){
-                                if (strtoupper($op[0]) == 'N'){
+                            echo 'MAIS UMA NOTA: [S] | [N] ' ; $Option = trim(fgets(STDIN));
+                            if(is_string($Option)){
+                                if (strtoupper($Option[0]) == 'N'){
                                     unset($n);
                                     $n = $media;
                                     $media = $nota/$n;
 
                                     $cadastro[$alunomedia]['Media'] = $media;
                                     break;
-                                } elseif(strtoupper($op[0]) == 'S'){
+                                } elseif(strtoupper($Option[0]) == 'S'){
                                     continue;
                                 }
                                 else{
-                                    echo 'MAIS UMA NOTA: [S] | [N] ' ; $op = trim(fgets(STDIN));
+                                    echo 'MAIS UMA NOTA: [S] | [N] ' ; $Option = trim(fgets(STDIN));
                                 }
                             }
 
@@ -86,22 +86,22 @@ if (is_string($op)){
         }
 
         }
-        elseif(strtoupper($op) == 'N'){
+        elseif(strtoupper($Option) == 'N'){
             echo 'OK';
         }
         else{
             echo 'Texto Invalido: ';
-            echo "Adicionar MEDIA a aluno [S] | [N]  " ; $op = trim(fgets(STDIN));
+            echo "Adicionar MEDIA a aluno [S] | [N]  " ; $Option = trim(fgets(STDIN));
         }
 }else{
     echo 'Texto Invalido: ' ; 
-    echo "Adicionar MEDIA a aluno [S] | [N] " ; $op = trim(fgets(STDIN)); 
+    echo "Adicionar MEDIA a aluno [S] | [N] " ; $Option = trim(fgets(STDIN)); 
 }
 unset($alunomedia);
 #Verificar e Retornar valor:
-echo 'Consulta de Aluno especifico [S] | [N] : ' ; $op = trim(fgets(STDIN));
-if(is_string($op)){
-    if (strtoupper($op[0]) == 'S'){
+echo 'Consulta de Aluno especifico [S] | [N] : ' ; $Option = trim(fgets(STDIN));
+if(is_string($Option)){
+    if (strtoupper($Option[0]) == 'S'){
         echo 'Nome do Aluno : ' ; $alunomedia = trim(fgets(STDIN));
         if (!isset($cadastro[$alunomedia])){
             echo "O Aluno $alunomedia  não existe";    
@@ -110,7 +110,7 @@ if(is_string($op)){
                 echo ucfirst($chave) . " : " . $value . PHP_EOL;
             }
         }
-    } elseif(strtoupper($op[0]) == 'N'){
+    } elseif(strtoupper($Option[0]) == 'N'){
         echo PHP_EOL . "--- LISTA DE ALUNOS ---" . PHP_EOL;
 
         foreach ($cadastro as $aluno => $dados) {
