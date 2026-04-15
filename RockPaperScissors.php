1435aff5c9ecd7bd81b32433af901373f83bab6c
MauricioDoria <mur1kin44@gmail.com>
2026-04-15 19:36:19 -0300

    Sintaxe Update

--- RockPaperScissors.php
+++ RockPaperScissors.php
@@ -2,11 +2,12 @@
 $opção  = ["PEDRA", "PAPEL", "TESOURA"];#Config IA
 $ia = random_int(0,2);
 if ($ia == 0){$ia='PEDRA';}elseif($ia == 1){$ia = 'PAPEL' ;}else{$ia = 'TESOURA';}
-
+echo "'PEDRA' 'PAPEL' 'TESOURA' " ;
 echo 'O que irar jogar' . PHP_EOL;$player=strtoupper((string)trim(fgets(STDIN)));
 #$player = strtoupper($player);
 
 while (in_array("$player", $opção) == false){
+    echo "'PEDRA' 'PAPEL' 'TESOURA' " ;
     echo 'O que irar jogar' . PHP_EOL;$player=(string)trim(fgets(STDIN));
     $player = strtoupper($player);
 }
@@ -14,7 +15,7 @@ echo '...' . PHP_EOL; sleep(2);
 
 if($ia == $player){echo "Ambos jogaram $ia o jogo deu Empate";}
 else{
-    if ($ia == 'PEDRA' and $player == 'TESOURA' or $ia == 'TESOURA' and $player == 'PAPEL' or $ia == 'PAPEL' and $player == 'PEDRA'){
+    if ($ia == 'PEDRA' && $player == 'TESOURA' || $ia == 'TESOURA' && $player == 'PAPEL' || $ia == 'PAPEL' && $player == 'PEDRA'){
         echo "Voce PERDEU\n"; 
     } else{echo "Voce GANHOU\n";}
 }
